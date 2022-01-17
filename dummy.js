1bc2de2f6cdbdfc4ec87b2f649eb9d5462ae1073
Nekonyx13 <corvin.prath@gmail.com>
2022-01-17 10:37:37 +0100

    full dataset displayed on page

--- dummy.js
+++ dummy.js
@@ -1,10 +1,8 @@
 'use strict';
 
-console.log('AAAAAAA')
-
 fetch('https://jsonplaceholder.typicode.com/todos/1')
-.then(response => response.json())
-.then(json => {
+    .then(response => response.json())
+    .then(json => {
     for (const key in json) {
         if (json.hasOwnProperty(key)) {
             document.getElementById(key).innerText = json[key];
